0ce0cc46e49511478e9d2f6664759325ffa55645
dani <groddpapyrus@yahoo.com>
2020-06-03 14:33:24 +0500

    node exercise1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var rect = {
 };
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + "and b = " + b);
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
     if (l <= 0 || b <= 0) {
         console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
@@ -16,4 +16,7 @@ function solveRect(l,b) {
     }
 }
 
-solveRect(2,4)
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);

7b71f3a913bdeda6ae107d5f1da4edb767e0560d
dani <groddpapyrus@yahoo.com>
2020-06-04 14:37:05 +0500

    coursera exercise 2

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,12 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) => {
+    
+
+}
+
+
+
+
+
+
+perimeter = (x,y) => (2*(x+y));
+area = (x,y) => (x*y);
